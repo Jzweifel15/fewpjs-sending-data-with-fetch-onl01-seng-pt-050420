57d7824233aa6b94a5d21e3eaeb0b5f6a75cddea
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-11-11 23:44:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 // Add your code here
 function submitData(userName, userEmail)
 {
+  let formData = {
+    name: userName,
+    email: userEmail
+  }
+  
   let configObj = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "Accept": "application/json"
     },
-    body: JSON.stringify({
-      name: userName,
-      email: userEmail
-    })
+    body: JSON.stringify(formData)
   };
   
-  return fetch("http://localhost:3000/users", configObj).then(function(resp) {
-    return resp.json();
-  }).then(function(obj) {
-    console.log(obj.id);
-  }).catch(function(error) {
-    console.log(error.message);
-  });
+  return fetch()
+  
 }
